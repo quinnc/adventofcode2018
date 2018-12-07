f1e055f1015e8eeed2d6d8d8606e823eb1c5e0e1
Quinn Liesch <q.liesch@gmail.com>
2018-12-07 09:20:47 -0700

    Initial commit

---
+++ day1.1/day1.1.cpp
@@ -0,0 +1,18 @@
+
+#include <cstdio>
+
+#include <iostream>
+
+
+int main (int argc, char** argv)
+{
+	if (argc != 1)
+	{
+		std::cout << "name the input file as a parameter!" << std::endl;
+		return -1;
+	}
+
+	
+
+	return 0;
+}
