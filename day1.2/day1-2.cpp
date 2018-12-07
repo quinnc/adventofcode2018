34bfd0dfe6edf8abd7d89a20cf06d48ca513154f
Quinn Liesch <q.liesch@gmail.com>
2018-12-07 11:24:42 -0700

    Removed prints and let it run until success

--- day1.2/day1-2.cpp
+++ day1.2/day1-2.cpp
@@ -39,7 +39,7 @@ int main (int argc, char** argv)
 	}
 	
 	long long curr_loc = 0;
-	int lines = 0;
+	unsigned long int lines = 0;
 	std::vector<long long int> totals_history;
 	std::vector<long long int> motion_history;
 	bool found_history_match = false;
@@ -53,7 +53,7 @@ int main (int argc, char** argv)
 	while (inputf.good())
 	{
 		motion_history.push_back(curr_motion);
-
+		lines++;
 		curr_loc += curr_motion;
 		auto i = totals_history.cbegin();
 		while (i != totals_history.cend() && (*i) != curr_loc)
@@ -73,20 +73,36 @@ int main (int argc, char** argv)
 		{
 			totals_history.push_back(curr_loc);
 		}
-		cout << "  + " << curr_motion << " = " << curr_loc;
-		lines++;
-		
-		inputf >> curr_motion;
-		if (lines % 10 == 0)
-			cout << endl;
+//		cout << "  + " << curr_motion << " = " << curr_loc;
+//		if (lines % 10 == 0)
+//			cout << endl;
 
+		inputf >> curr_motion;
 	}
 
+	cout << endl;
+	cout << "History sizes, totals: " << totals_history.size() << ", motions: " << motion_history.size() << endl;
+	cout << "Total lines processed = " << lines << endl;
+	cout << endl;
+
+		if (totals_history.size() != motion_history.size() + 1)
+		{
+			cout << "History vectors are not the same size!" << endl;
+
+		}
+
+		if (motion_history.size() != lines || totals_history.size() != lines+1)
+		{
+			cout << "History vectors do not match the number of lines processed!" << endl;
+		}
+	
 	while (!found_history_match)
 	{
 		for (auto p_curr_motion = motion_history.cbegin(); p_curr_motion != motion_history.cend(); p_curr_motion++)
 		{
 	             curr_loc += (*p_curr_motion);
+			lines++;
+
                 auto i = totals_history.cbegin();
                 while (i != totals_history.cend() && (*i) != curr_loc)
                         i++;
@@ -103,16 +119,23 @@ int main (int argc, char** argv)
                 {
                         totals_history.push_back(curr_loc);
                 }
-              cout << "  + " << *p_curr_motion << " = " << curr_loc;
-                lines++;
+  //            cout << "  + " << *p_curr_motion << " = " << curr_loc;
 
-                if (lines % 10 == 0)
-                        cout << "   [lines processed: " << lines << "] " << endl;
+  //              if (lines % 10 == 0)
+  //                      cout << "   [lines processed: " << lines << "] " << endl;
+
+
 		if (found_history_match)
+{
+		cout << "leaving loop because match found!" << endl;
 			break;
-
-		if (lines > 5000)
-			break;
+}
+	
+//		if (lines > 20000)
+//	{
+//		cout << "leaving loop because processed too much!" << endl;
+//		break;
+//		}
 		}
         }
 
@@ -122,6 +145,18 @@ int main (int argc, char** argv)
 	cout << "Total lines processed: " << lines << endl;
 	cout << "First location repetition: " << match_value << endl;
 	cout << "Found a repetition? " << found_history_match << endl;	
+     cout << endl;
+        cout << "History sizes, totals: " << totals_history.size() << ", motions: " << motion_history.size() << endl;
+        cout << "Total lines processed = " << lines << endl;
+        cout << endl;
+
+                if (totals_history.size() != lines+1)
+                {
+                        cout << "History vectors do not match the number of lines processed!" << endl;
+                }
+
+	cout << endl;
+
 	if (inputf.eof())
 	{
 		cout << "Ended because of EOF" << endl;
